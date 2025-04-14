d80fa603b6e9ecbe0d936a954ee756dc50065a08
Alimurod <alimurodakbarov@gmail.com>
2025-04-14 10:12:23 +0500

    accounts, book, wishlist apps complated

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -7,14 +7,6 @@ from .views import (BookListAPIView,
                     BookUploadImageAPIView
 )
 
-# 1. `GET /api/books/` – Barcha kitoblarni olish
-# 2. `POST /api/books/` – Yangi kitob qo‘shish (admin)
-# 3. `POST /api/books/{id}/upload-image/` – Kitob uchun rasm yuklash
-# 4. `GET /api/books/{id}/` – Bitta kitobni olish
-# 5. `PUT /api/books/{id}/` – Kitobni yangilash (admin)
-# 6. `DELETE /api/books/{id}/` – Kitobni o‘chirish (admin)
-# 7. `GET /api/books/search/?q=...` – Kitoblarni nomi yoki muallifiga qarab qidirish
-
 urlpatterns = [
     path('', BookListAPIView.as_view(), name='books'),
     path('create/', BookCreateAPIView.as_view(), name='book_create'),
